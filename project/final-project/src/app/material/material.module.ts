ea4405209eb54211493c81e6f6f038b43d505a47
Toma Cristian <i.toma_cristian@yahoo.com>
2021-12-27 21:38:19 +0200

    background images for about and resume

--- project/final-project/src/app/material/material.module.ts
+++ project/final-project/src/app/material/material.module.ts
@@ -31,7 +31,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatButtonModule,
     MatListModule,
     MatMenuModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
 })
 export class MaterialModule {}
