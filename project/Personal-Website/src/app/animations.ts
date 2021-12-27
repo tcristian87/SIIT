7c80c266c7cd751bc95903b3a12c887962cf7146
Toma Cristian <i.toma_cristian@yahoo.com>
2021-12-27 08:46:39 +0200

    adding material and libraries

--- project/Personal-Website/src/app/animations.ts
+++
@@ -1,8 +0,0 @@
-import { trigger, transition, style, animate } from '@angular/animations' ;
-
-export const fadeAnimation = trigger('fade', [
-      transition('void =>*', [
-        style({ opacity: 0 }),
-        animate(2000)
-      ])
-    ])
